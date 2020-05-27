fa4120f7afc51032c0fc85f089f639fbe7a38914
Stefan Kalscheuer <stefan@stklcode.de>
2020-05-27 11:25:23 +0200

    prepare release 2.3.1
    * update version and stable tag
    * CHANGELOG++
    * README++
    * Update screenshot of settings page

--- cachify.php
+++ cachify.php
@@ -7,7 +7,7 @@ Author URI:  https://pluginkollektiv.org
 Plugin URI:  https://cachify.pluginkollektiv.org/
 License:     GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Version:     2.3.0
+Version:     2.3.1
 Text Domain: cachify
 Domain Path: /lang
 */
